818136ee1aa1d5219fb9f091f44e21395cb462ba
Deepanshu <100932118+calmdeep@users.noreply.github.com>
2025-11-16 07:22:38 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        orange: {
+          500: '#f97316',
+          600: '#ea580c',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
